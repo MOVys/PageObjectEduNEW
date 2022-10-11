464a7d8fb382859b9cd724444ea0f93a152e2280
Marina Vysotskaya <vippmar@gmail.com>
2022-10-11 13:09:39 +0300

    Произведена работа над ошибками, добавлены проверки цены и названия книг в корзине

--- pages/locators.py
+++ pages/locators.py
@@ -9,6 +9,12 @@ class LoginPageLocators:
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")        
     
 class BtnAddToBasketLocators:
-    ADD_BUTTON = (By.CSS_SELECTOR, 'button.btn-add-to-basket') 
+    ADD_BUTTON = (By.CSS_SELECTOR, 'button.btn-add-to-basket')
+    BOOK_NAME = (By.TAG_NAME, 'h1')
+    BOOK_PRICE = (By.CSS_SELECTOR, "p.price_color")
+    BASKET_PRICE = (By.CSS_SELECTOR, ".alertinner p strong")
+    BASKET_BOOKNAME = (By.CSS_SELECTOR, "div.alertinner ")
+
+#content_inner > article > div.row > div.col-sm-6.product_main > h1    
 
 
--- pages/product_page.py
+++ pages/product_page.py
@@ -3,5 +3,23 @@ from .locators import MainPageLocators
 from .locators import BtnAddToBasketLocators
 
 class ProductPage(BasePage):
-    def click_basket_button(self):
+    def should_be_buy_book(self):
+        self.add_to_basket()
+        self.should_price_check()
+        self.should_name_check()
+        
+    def add_to_basket(self):
         self.browser.find_element(*BtnAddToBasketLocators.ADD_BUTTON).click()
+        self.solve_quiz_and_get_code()
+        
+    
+    def should_price_check(self):
+        product_price_check = self.browser.find_element(*BtnAddToBasketLocators.BOOK_PRICE)
+        basket_price_check = self.browser.find_element(*BtnAddToBasketLocators.BASKET_PRICE)
+        assert basket_price_check.text == product_price_check.text,"Цены не совпадают"
+
+    def should_name_check(self):
+        book_name = self.browser.find_element(*BtnAddToBasketLocators.BOOK_NAME)
+        basket_name = self.browser.find_element(*BtnAddToBasketLocators.BASKET_BOOKNAME)
+        print(basket_name, book_name)
+        assert book_name.text in basket_name.text, "Названия книг не совпадают"
